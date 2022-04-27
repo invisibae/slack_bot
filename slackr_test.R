692c99f1f6826daf1c6bba75bc388459db64d4a9
Greg Morton <gregorymorton96@gmail.com>
2022-04-27 14:18:56 -0400

    test test

--- slackr_test.R
+++ slackr_test.R
@@ -7,9 +7,7 @@ slackr_setup(
              token = Sys.getenv("SLACK_TOKEN"),
              channel = Sys.getenv("SLACK_CHANNEL"),
              username = Sys.getenv("SLACK_USERNAME"),
-             icon_emoji = Sys.getenv("SLACK_ICON_EMOJI"),
-             thread_ts = NULL,
-             reply_broadcast = FALSE
+             icon_emoji = Sys.getenv("SLACK_ICON_EMOJI")
 )
 
 slackr_msg(txt = this_week_allegations,
