857f73712046e176d28917184dba249cdb9af145
Greg Morton <gregorymorton96@gmail.com>
2022-04-27 14:28:48 -0400

    TEST TEST TEST

--- slackr_test.R
+++ slackr_test.R
@@ -4,11 +4,14 @@ library(slackr)
 test <- "this is a test message"
 
 slackr_setup(
-             token = Sys.getenv("SLACK_TOKEN"),
-             channel = Sys.getenv("SLACK_CHANNEL"),
-             username = Sys.getenv("SLACK_USERNAME"),
-             icon_emoji = Sys.getenv("SLACK_ICON_EMOJI")
+  channel = Sys.getenv("SLACK_CHANNEL"),
+  username = Sys.getenv("SLACK_USERNAME"),
+  icon_emoji = Sys.getenv("SLACK_ICON_EMOJI"),
+  incoming_webhook_url = Sys.getenv("SLACK_INCOMING_WEBHOOK_URL"),
+  token = Sys.getenv("SLACK_TOKEN")
 )
+
+
 
 slackr_msg(txt = this_week_allegations,
            token = Sys.getenv("SLACK_TOKEN"),
